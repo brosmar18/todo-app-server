4254cc6f1d51c1e8c36855a85a9bae31f9a8b7d5
brosmar18 <bryangonzalez1896@icloud.com>
2024-03-09 14:33:53 -0600

    Successfully connected to frontend using cors

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 "use strict";
 
 const express = require("express");
+const logger = require("./Utils/Logger");
+const cors = require("cors");
 const PORT = process.env.PORT || 5002;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res, next) => {
   res.status(200).send("Hello World!");
 });
 
 const start = () => {
-  app.listen(PORT, () => console.log("Listening on PORT: ", PORT));
+  app.listen(PORT, () => logger.info(`Server is running on PORT: ${PORT}`));
 };
 
 module.exports = {
