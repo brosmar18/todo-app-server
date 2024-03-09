1f8841402d79018d72a486c3d929b79552f96be0
brosmar18 <bryangonzalez1896@icloud.com>
2024-03-09 14:50:28 -0600

    basic user model created

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,26 @@
 "use strict";
 
-module.exports = (sequelizeDatabase, DataTypes) => {
-  return sequelizeDatabase.define("User", {
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    {
+      hooks: {
+        beforeCreate: (user) => {
+          console.log("Creating user:", JSON.stringify(user, null, 2));
+        },
+      },
+    }
+  );
+  return User;
 };
--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,17 @@
 "use strict";
 
 const { Sequelize, DataTypes } = require("sequelize");
-const User = require("./User");
-
+const user = require("./User");
 require("dotenv").config();
+
 const DATABASE_URL =
   process.env.NODE_ENV === "test" ? "sqlite::memory" : process.env.DATABASE_URL;
 
 const sequelizeDatabase = new Sequelize(DATABASE_URL);
 
-const usersModel = User(sequelizeDatabase, DataTypes);
+const userModel = user(sequelizeDatabase, DataTypes);
 
 module.exports = {
   sequelizeDatabase,
-  usersModel,
+  userModel, 
 };
