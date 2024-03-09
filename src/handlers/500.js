043df8a504049ab452f47e8095ca274e7652c24a
brosmar18 <bryangonzalez1896@icloud.com>
2024-03-09 14:43:34 -0600

    500 errorHandler added

---
+++ src/handlers/500.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const logger = require("../Utils/Logger");
+
+module.exports = (err, req, res, next) => {
+  logger.error(
+    `500 - Server Error - ${req.method} ${req.path} - Error message: ${err.message}`,
+    {
+      error: err,
+      route: req.path,
+      query: req.query,
+      body: req.body,
+    }
+  );
+
+  res.status(500).json({
+    error: 500,
+    route: req.path,
+    message: "An unexpected error occurred on the server.",
+  });
+};
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5002;
 
 // Error Handlers
 const notFound = require("./handlers/404");
+const errorHandler = require("./handlers/500");
 
 const app = express();
 app.use(express.json());
@@ -16,7 +17,12 @@ app.get("/", (req, res, next) => {
   res.status(200).send("Hello World!");
 });
 
+app.get("/error", (req, res, next) => {
+  throw new Error("Forced Error For Testing");
+});
+
 app.use("*", notFound);
+app.use(errorHandler);
 
 const start = () => {
   app.listen(PORT, () => logger.info(`Server is running on PORT: ${PORT}`));
